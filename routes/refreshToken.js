e2605c2c06694728bbd1348ef8b4e5f2c310a849
Rhitam Banerjee <rb2403@it.jgec.ac.in>
2024-02-13 12:05:15 +0530

    added comments in refreshToken.js

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -7,17 +7,24 @@ const { refreshTokenBodyValidation } =require("../utils/validationSchema.js");
 const router = Router();
 dotnenv.config();
 
-// get new access token
+//Route for getting a new access token using a refresh token
 router.post("/", async (req, res) => {
+	// Validating the request body for refresh token
 	const { error } = refreshTokenBodyValidation(req.body);
 	if (error)
 		return res
 			.status(400)
 			.json({ error: true, message: error.details[0].message });
 
+
+	// Verifying the refresh token
 	verifyRefreshToken(req.body.refreshToken)
 		.then(({ tokenDetails }) => {
-			const payload = { _id: tokenDetails._id, roles: tokenDetails.roles };
+
+			// Creating payload for new access token
+			const payload = { _id: tokenDetails._id};
+
+			// Signing a new access token with the payload
 			const accessToken = jwt.sign(
 				payload,
 				process.env.ACCESS_TOKEN_PRIVATE_KEY,
@@ -32,7 +39,7 @@ router.post("/", async (req, res) => {
 		.catch((err) => res.status(400).json(err));
 });
 
-// logout
+// Route for logging out (deleting refresh token)
 router.delete("/", async (req, res) => {
 	try {
 		const { error } = refreshTokenBodyValidation(req.body);
@@ -41,12 +48,14 @@ router.delete("/", async (req, res) => {
 				.status(400)
 				.json({ error: true, message: error.details[0].message });
 
+		// Finding the user token associated with the refresh token
 		const userToken = await UserToken.findOne({ token: req.body.refreshToken });
 		if (!userToken)
 			return res
 				.status(200)
 				.json({ error: false, message: "Logged Out Sucessfully" });
 
+		// Removing the user token from the database
 		await userToken.remove();
 		res.status(200).json({ error: false, message: "Logged Out Sucessfully" });
 	} catch (err) {
