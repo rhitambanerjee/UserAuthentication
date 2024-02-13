1ca6ad876318e8f2985678e13e550316349fde74
Rhitam Banerjee <rb2403@it.jgec.ac.in>
2024-02-13 12:02:29 +0530

    added comments in dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -5,7 +5,11 @@ const dotenv=require('dotenv');
 const dbConnect = async ()=>{
     dotenv.config();
     try {
+
+        // Connecting to the MongoDB database using the connection string from environment variables
         mongoose.connect(process.env.DB);
+
+        // Event listener for successful connection
         mongoose.connection.on("connected",()=>{
             console.log('DB connected successfully');
         })
